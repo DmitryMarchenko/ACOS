4f3d7f01ced07f5a7337c4334be4b839b5d582c6
marchenko7 <mds98@yandex.ru>
2018-09-24 22:31:31 +0300

    solution of hw2

--- Onegin/main.cpp
+++ Onegin/main.cpp
@@ -2,37 +2,49 @@
 #include <fstream>
 #include <algorithm>
 #include <cstring>
+#include <cstdio>
+#include <sys/stat.h>
+#include <cstdlib>
+#include <cctype>
 
 struct line {
     line() : str(nullptr), len(0) {}
     char* str;
     size_t len;
+
+    char* ptr() const {
+        return str;
+    }
+
+    size_t size() const {
+        return len;
+    }
 };
 
-line* find_begins_of_lines(char* buffer, size_t buffer_size, size_t& endl_count) {
+line* find_lines(char* buffer, size_t buffer_size, size_t& lines_count) {
     for (int i = 0; i < buffer_size; ++i) {
         if (buffer[i] == '\n') {
-            ++endl_count;
+            ++lines_count;
         }
     }
 
-    line* begins_of_lines = new line[endl_count];
-    begins_of_lines[0].str = buffer;
+    line* lines = new line[lines_count];
+    lines[0].str = buffer;
     int j = 1;
 
-    for (int i = 0; i < buffer_size && j < endl_count + 1; ++i) {
+    for (int i = 0; i < buffer_size && j < lines_count + 1; ++i) {
         if (buffer[i] == '\n') {
             if (i < buffer_size - 1) {
-                begins_of_lines[j].str = buffer + i + 1;
+                lines[j].str = buffer + i + 1;
             }
-            begins_of_lines[j - 1].len = begins_of_lines[j].str - begins_of_lines[j - 1].str - 1;
+            lines[j - 1].len = lines[j].ptr() - lines[j - 1].ptr() - 1;
             ++j;
             buffer[i] = '\0';
         }
     }
-    begins_of_lines[j - 2].len = buffer + buffer_size - begins_of_lines[j - 2].str - 1;
+    lines[j - 2].len = buffer + buffer_size - lines[j - 2].ptr() - 1;
 
-    return begins_of_lines;
+    return lines;
 }
 
 size_t to_str(const char* a) {
@@ -47,22 +59,22 @@ size_t to_str(const char* a) {
 }
 
 bool comparator(const line& a, const line& b) {
-    size_t a_i = to_str(a.str);
-    size_t b_i = to_str(b.str);
-    while (a.str[a_i] != '\0' && b.str[b_i] != '\0' && a.str[a_i] == b.str[b_i]) {
+    size_t a_i = to_str(a.ptr());
+    size_t b_i = to_str(b.ptr());
+    while (a.ptr()[a_i] != '\0' && b.ptr()[b_i] != '\0' && tolower(a.ptr()[a_i]) == tolower(b.ptr()[b_i])) {
         ++a_i;
         ++b_i;
     }
-    return (a.str[a_i] == '\0' && b.str[b_i] != '\0') || a.str[a_i] < b.str[b_i];
+    return (a.ptr()[a_i] == '\0' && b.ptr()[b_i] != '\0') || tolower(a.ptr()[a_i]) < tolower(b.ptr()[b_i]);
 }
 
 size_t return_to_str(const line& a) {
     if (&a == nullptr) {
         return 0;
     }
-    size_t pos = a.len - 1;
-    while (!(a.str[pos] >= 'A' && a.str[pos] <= 'Z') && !(a.str[pos] >= 'a' && a.str[pos] <= 'z')
-           && &a.str[pos] != a.str) {
+    size_t pos = a.size() - 1;
+    while (!(a.ptr()[pos] >= 'A' && a.ptr()[pos] <= 'Z') && !(a.ptr()[pos] >= 'a' && a.ptr()[pos] <= 'z')
+           && &a.ptr()[pos] != a.ptr()) {
         --pos;
     }
     return pos;
@@ -72,16 +84,19 @@ size_t return_to_str(const line& a) {
 bool reverse_comparator(const line& a, const line& b) {
     size_t a_i = return_to_str(a);
     size_t b_i = return_to_str(b);
-    while (&a.str[a_i] != a.str && &b.str[b_i] != b.str && a.str[a_i] == b.str[b_i]) {
+    while (&a.ptr()[a_i] != a.ptr() && &b.ptr()[b_i] != b.ptr() && tolower(a.ptr()[a_i]) == tolower(b.ptr()[b_i])) {
         --a_i;
         --b_i;
     }
-    return (&a.str[a_i] == a.str && &b.str[b_i] != b.str) || a.str[a_i] < b.str[b_i];
+    return (&a.ptr()[a_i] == a.ptr() && &b.ptr()[b_i] != b.ptr()) || tolower(a.ptr()[a_i]) < tolower(b.ptr()[b_i]);
 }
 
 void print_lines(const line* lines, size_t lines_count, std::FILE* file) {
     for (int i = 0; i < lines_count; ++i) {
-        std::fwrite(lines[i].str, sizeof(char), lines[i].len, file);
+        if (lines[i].size() == 0) {
+            continue;
+        }
+        std::fwrite(lines[i].ptr(), sizeof(char), lines[i].size(), file);
         std::fwrite("\n", sizeof('\n'), sizeof('\n'), file);
     }
 }
@@ -94,36 +109,50 @@ void recover_buffer(char* buffer, size_t buffer_size) {
     }
 }
 
-int main() {
-    std::FILE* f = fopen("input.txt", "r");
-    std::fseek(f, 0, SEEK_END);
-    auto file_size = static_cast<size_t>(std::ftell(f));
+char* to_buffer(const char* input_file_name, size_t& file_size) {
+    std::FILE* f = fopen(input_file_name, "r");
 
-    std::fseek(f, 3, SEEK_SET);//fstat
+    struct stat file_info;
+    fstat(fileno(f), & file_info);
+    file_size = static_cast<size_t>(file_info.st_size);
+
     char* buffer = new char[file_size];
     std::fread(buffer, sizeof(char), file_size, f);
+
     std::fclose(f);
 
-    size_t endl_count = 0;
-    line* begins_of_lines = find_begins_of_lines(buffer, file_size, endl_count);
+    return buffer;
+}
 
-    std::sort(begins_of_lines, begins_of_lines + endl_count, comparator);
+void print_sorted_lines(const line* lines, size_t lines_count, const char* output_file_name) {
+    std::FILE* f = fopen(output_file_name, "w");
+    print_lines(lines, lines_count, f);
+    std::fclose(f);
+}
 
-    std::FILE* f1 = fopen("output1.txt", "w");
-    print_lines(begins_of_lines, endl_count, f1);
-    std::fclose(f1);
-
-    std::sort(begins_of_lines, begins_of_lines + endl_count, reverse_comparator);
-
-    std::FILE* f2 = fopen("output2.txt", "w");
-    print_lines(begins_of_lines, endl_count, f2);
-    std::fclose(f2);
-
+void print_original(char* buffer, size_t file_size, const char* output_file_name) {
     recover_buffer(buffer, file_size);
+    std::FILE* f = fopen(output_file_name, "w");
+    std::fwrite(buffer, sizeof(char), file_size, f);
+    std::fclose(f);
+}
 
-    std::FILE* f3 = fopen("output3.txt", "w");
-    std::fwrite(buffer, sizeof(char), file_size, f3);
-    std::fclose(f3);
+int main() {
+    size_t file_size = 0;
+    char* buffer = to_buffer("input.txt", file_size);
+
+    size_t lines_count = 0;
+    line* lines = find_lines(buffer, file_size, lines_count);
+
+    std::sort(lines, lines + lines_count, comparator);
+
+    print_sorted_lines(lines, lines_count, "output1.txt");
+
+    std::sort(lines, lines + lines_count, reverse_comparator);
+
+    print_sorted_lines(lines, lines_count, "output2.txt");
+
+    print_original(buffer, file_size, "output3.txt");
 
     return 0;
 }
